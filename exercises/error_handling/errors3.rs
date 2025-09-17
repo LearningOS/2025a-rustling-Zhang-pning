ee2821bd094ec2820db54700d568388667c2d35e
Zhang-pning <2325931355@qq.com>
2025-09-17 22:29:46 +0800

    update

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -15,13 +15,16 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+    match total_cost(pretend_user_input) {
+        Ok(n) => {
+            if n > tokens {
+                println!("You can't afford that many!");
+            } else {
+                tokens -= n;
+                println!("You now have {} tokens.", tokens);
+            }
+        }
+        Err(e) => println!("Couldn't parse the input: {}", e),
     }
 }
 
